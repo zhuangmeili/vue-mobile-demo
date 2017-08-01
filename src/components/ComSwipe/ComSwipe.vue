00fef8789345dee5d1ea2859e94a0948d83eae84
zhuangmeili <zhuang493405329@163.com>
2017-08-01 18:07:21 +0800

    swiper

--- src/components/ComSwipe/ComSwipe.vue
+++ src/components/ComSwipe/ComSwipe.vue
@@ -1,20 +1,66 @@
 <template>
   <div class="com-swiper">
-    <!-- 滚动区域  -->
-    <div class="com-swiper-wrap" ref="wrap">
-      <slot name="content"></slot>
+    <!-- pics  -->
+    <div ref="swipePics">
+      <slot name="pics" ></slot>
     </div>
-    <!-- dot -->
-    <div class="com-swiper-dots">yuandian </div>
+    <!-- dots -->
+    <div ref="swipeDots">
+      <slot name="dots"></slot>
+    </div>
   </div>
 </template>
 <script type="text/javascript">
   export default{
     name:'com-swipe',
     data(){
-      return {}
-    },
+      return {
+        startX:0,
+        endX:0,
+        disX:0,
+        oPics:null,
+        oPicLis:null,
+        oImgs:null,
 
+        oDots:null,
+        oDotLis:null,
+        len:0,
+        stepW:0,
+        maxMove:0,
+        curPage:0,
+        isMove:false, //是否运动
+
+        startTime:0,
+        endTime:0,
+        timer:null,
+      }
+    },
+    methods:{
+      initSwipe(){
+        //初始化
+        this.oPics = this.$refs.swipePics;
+        this.oPicLis=this.oPics.getElementsByTagName("li");
+        this.oImgs = this.oPics.getElementsByTagName("img");
+        this.oDots=this.$refs.swipeDots;
+
+        this.oDotLis=this.oDots.getElementsByTagName("li");
+        this.len = this.oPics.getElementsByTagName("li").length/2;
+        this.stepW = this.oPics.getBoundingClientRect().width;
+        this.maxMove = Math.floor(this.stepW / 4);
+        for (let i = 0; i < this.len; i++) {
+          this.oImgs[i].style.width = this.stepW + 'px';
+          //this.oDotLis[i].className="";
+        }
+        //this.oDotLis[0].className="cur";
+      },
+    },
+    mounted(){
+      this.$nextTick(function () {
+        //DOM渲染完成调用swipe方法
+        this.initSwipe();
+
+      })
+    }
   }
 </script>
 <style scoped="scoped">
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,12 +5,16 @@
         <router-link class="link" to="/swiper">swiper</router-link>
       </li>
     </ul>
-    <com-swipe>
-      <ul slot="content" ref="swipe">
-        <li v-for="item in banners" ref="swipe-item">
+    <com-swipe class="swiper">
+      <!-- pics -->
+      <ul slot="pics" class="pics" >
+        <li v-for="item in banners" class="swipe-item" >
           <img :src="item.imgUrl" alt="" class="bannerImg">
         </li>
       </ul>
+      <ol slot="dots" class="dots" >
+        <li v-for="i in dotsNumber">{{i}}</li>
+      </ol>
     </com-swipe>
   </div>
 </template>
@@ -19,11 +23,22 @@
     data(){
       return {
         banners:[
+          //前台处理一下，让数据变成原来的二倍（后台反馈的是4条，前台处理一下*2倍；）
+          {id:1,imgUrl:'../static/fodder/test1.jpg'},
+          {id:2,imgUrl:'../static/fodder/test2.jpg'},
+          {id:3,imgUrl:'../static/fodder/test3.jpg'},
+          {id:4,imgUrl:'../static/fodder/test4.jpg'},
           {id:1,imgUrl:'../static/fodder/test1.jpg'},
           {id:2,imgUrl:'../static/fodder/test2.jpg'},
           {id:3,imgUrl:'../static/fodder/test3.jpg'},
           {id:4,imgUrl:'../static/fodder/test4.jpg'}
-        ]
+        ],
+
+      }
+    },
+    computed:{
+      dotsNumber(){
+        return this.banners.length/2;
       }
     }
   }
@@ -32,4 +47,29 @@
   .link{ height: 3rem; font-size: 2rem; text-align: center; display: block; line-height: 3rem;
     border-bottom: 1px solid #ddd;}
   .bannerImg{display: block; width: 100%; height: 15rem;}
+
+
+  .swiper {
+    position: relative;
+    width: 100%;
+    height: 20rem;
+    overflow: hidden;
+  }
+
+  .pics {
+    display: flex;
+    display: -webkit-flex;
+    /*transition:cubic-bezier(0.59, -0.03, 0.28, 1) 0.4s;*/
+    /*-webkit-transition:cubic-bezier(0.59, -0.03, 0.28, 1) 0.4s ;*/
+
+
+  }
+
+  .pics li {  flex: 1;  }
+
+  .pics li img {  height: 20rem;  display: block;  }
+  .dots{ width: 100%; position: absolute; left: 0; bottom: 10px; z-index: 9;  text-align: center;  }
+  .dots li{ display: inline-block; width: 20px; height: 20px; background: rgba(0,0,0,0.2);  margin:0 5px;
+    border-radius: 50%; text-indent: 100px; overflow: hidden;}
+  .dots li.cur{ background: rgba(37, 224, 5, 0.8);;}
 </style>
