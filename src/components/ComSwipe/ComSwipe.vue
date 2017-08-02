63a788d566224bde1f78da3f52e88ae9a1104879
zhuangmeili <zhuang493405329@163.com>
2017-08-02 16:22:28 +0800

    swiper基本完成

--- src/components/ComSwipe/ComSwipe.vue
+++ src/components/ComSwipe/ComSwipe.vue
@@ -35,6 +35,7 @@
         timer:null,
       }
     },
+    props:['auto','interval'],
     methods:{
       initSwipe(){
         //初始化
@@ -47,7 +48,7 @@
         this.len = this.oPics.getElementsByTagName("li").length/2;
         this.stepW = this.oPics.getBoundingClientRect().width;
         this.maxMove = Math.floor(this.stepW / 4);
-        for (let i = 0; i < this.len; i++) {
+        for (let i = 0; i < (this.len)*2; i++) {
           this.oImgs[i].style.width = this.stepW + 'px';
           //this.oDotLis[i].className="";
         }
@@ -63,6 +64,9 @@
         this.oPics.style.WebkitTransition = 'cubic-bezier(0.59, -0.03, 0.28, 1) 0.4s ';
         this.oPics.style.transform='translateX('+(-this.curPage*this.stepW)+'px)';
         this.oPics.style.WebkitTransform = 'translateX('+(-this.curPage*this.stepW)+'px)';
+      },
+      test(){
+        console.log("get Test方法");
       },
       // 改变焦点
       changeTabFocus() {
@@ -80,30 +84,106 @@
           this.oPics.style.WebkitTransform = 'translateX('+(-this.curPage*this.stepW)+'px)';
         }
       },
+      touchStart(e){
+        clearInterval(this.timer);//清除timer
+        console.log("timer clear"+this.timer);
+        this.startTime = (new Date()).getTime();
+        this.startX = this.getX(e);
+        this.isMove = true;
+        return false;
+      },
+      touchMove(e){
+        if (!this.isMove) {
+          return false;
+        }
+        this.endX = this.getX(e);
+        this.disX = this.endX - this.startX;
+        if(this.disX<0){ //left
+
+        }
+        if(this.disX>0){ //drag right
+          if (this.curPage==0){
+            this.curPage=this.len;
+          }
+        }
+        this.oPics.style.transition='cubic-bezier(0.59, -0.03, 0.28, 1) 0s ';
+        this.oPics.style.WebkitTransition = 'cubic-bezier(0.59, -0.03, 0.28, 1) 0s ';
+        this.oPics.style.transform = 'translateX(' + (this.disX-this.curPage*this.stepW) + 'px)';
+        this.oPics.style.WebkitTransform = 'translateX(' + (this.disX-this.curPage*this.stepW) + 'px)';
+        return false;
+      },
+      touchEnd(e){
+        var _this=this;
+        if(!this.isMove){ return false; }
+        this.isMove=false;
+        this.endTime=(new Date()).getTime();
+        var speed=Math.abs(this.startX-this.endX)/(this.endTime-this.startTime);
+
+        // 左滑动 left (1)滑动的慢 但是超过1/4 则表示划过去了（2）速度比较快也表示划过去了
+        if (this.disX<0) {
+          if(Math.abs(this.disX)>this.maxMove ||speed>0.4 ){
+            //翻页
+            this.curPage++;
+            if(this.curPage>=this.len){
+              this.showPics();
+              this.curPage=this.curPage%this.len;
+            }else{
+              this.showPics();
+            }
+            console.log(this.curPage);
+            this.changeTabFocus(); //焦点变化
+          }else{
+            this.showPics();
+          }
+        }
+        // 右滑动 right
+        if(this.disX>0){
+          if(Math.abs(this.disX)>this.maxMove ||speed>0.4 ){
+            this.curPage--;
+            this.showPics();
+            this.changeTabFocus(); //焦点变化
+          }else{
+            this.showPics();
+          }
+        }
+        if(!this.timer || this.auto){
+          console.log("timer set!!设置timer");
+          this.autoPlayLeft();
+        }
+        return false;
+      },
       //自动向左边的轮播
       autoPlayLeft(){
-        if(this.autoPlay){
-          //oPics.addEventListener("transitionend", transitionEndFn);
-          timer=setInterval(function () {
-            curPage++;
-            if(curPage>=len/2){
+        var _this=this;
+        if(this.auto){
+          _this.timer=setInterval(function () {
+            _this.curPage++;
+            if(_this.curPage>=_this.len){
               //1、先运动到最后
-              showPics();
+              _this.showPics();
               //2、curPage  //一瞬间拉过来
-              curPage=curPage%(len/2);
+              _this.curPage=_this.curPage%_this.len;
             }else{
-              showPics();
+              _this.showPics();
             }
-            changeTabFocus(); //焦点变化
-          },intervalTime);
+            _this.changeTabFocus(); //焦点变化
+          },_this.interval);
         }
+      },
+      swipe(){
+        var _this=this;
+        this.initSwipe(); // 初始化swipe
+        this.oPics.addEventListener("transitionend", this.transitionEndFn);
+        this.oPics.addEventListener('touchstart', function (e){_this.touchStart(e);});
+        this.oPics.addEventListener('touchmove', function (e){_this.touchMove(e);});
+        this.oPics.addEventListener('touchend', function (e){_this.touchEnd(e);});
+        this.autoPlayLeft();  //调用autoplay
+
       }
     },
     mounted(){
       this.$nextTick(function () {
-        //DOM渲染完成调用swipe方法
-        this.initSwipe();
-
+        this.swipe();
       })
     }
   }
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,7 +5,7 @@
         <router-link class="link" to="/swiper">swiper</router-link>
       </li>
     </ul>
-    <com-swipe class="swiper" :auto-play="true">
+    <com-swipe class="swiper" :auto="true" :interval="3000">
       <!-- pics -->
       <ul slot="pics" class="pics" >
         <li v-for="item in banners" class="swipe-item" >
@@ -13,7 +13,7 @@
         </li>
       </ul>
       <ol slot="dots" class="dots" >
-        <li v-for="i in dotsNumber">{{i}}</li>
+        <li v-for="i in dotsNumber" :class="{cur:dotIndex==i}">{{i}}</li>
       </ol>
     </com-swipe>
   </div>
@@ -22,16 +22,17 @@
   export default{
     data(){
       return {
+        dotIndex:1,
         banners:[
           //前台处理一下，让数据变成原来的二倍（后台反馈的是4条，前台处理一下*2倍；）
           {id:1,imgUrl:'../static/fodder/test1.jpg'},
           {id:2,imgUrl:'../static/fodder/test2.jpg'},
           {id:3,imgUrl:'../static/fodder/test3.jpg'},
-          {id:4,imgUrl:'../static/fodder/test4.jpg'},
+
           {id:1,imgUrl:'../static/fodder/test1.jpg'},
           {id:2,imgUrl:'../static/fodder/test2.jpg'},
           {id:3,imgUrl:'../static/fodder/test3.jpg'},
-          {id:4,imgUrl:'../static/fodder/test4.jpg'}
+
         ],
 
       }
@@ -62,14 +63,13 @@
     /*transition:cubic-bezier(0.59, -0.03, 0.28, 1) 0.4s;*/
     /*-webkit-transition:cubic-bezier(0.59, -0.03, 0.28, 1) 0.4s ;*/
 
-
   }
 
   .pics li {  flex: 1;  }
 
   .pics li img {  height: 20rem;  display: block;  }
-  .dots{ width: 100%; position: absolute; left: 0; bottom: 10px; z-index: 9;  text-align: center;  }
-  .dots li{ display: inline-block; width: 20px; height: 20px; background: rgba(0,0,0,0.2);  margin:0 5px;
+  .dots{ width: 100%; position: absolute; left: 0; bottom: 0.5rem; z-index: 9;  text-align: center;  }
+  .dots li{ display: inline-block; width: 1rem; height: 1rem; background: rgba(0,0,0,0.2);  margin:0 0.5rem;
     border-radius: 50%; text-indent: 100px; overflow: hidden;}
   .dots li.cur{ background: rgba(37, 224, 5, 0.8);;}
 </style>
