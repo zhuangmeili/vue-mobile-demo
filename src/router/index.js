a120b7395e2525359491c1c84c49d4e3f7c23f1a
zhuangmeili <zhuangmeili@99114.com>
2017-09-04 09:43:57 +0800

    ajaj

--- src/router/index.js
+++ src/router/index.js
@@ -6,11 +6,27 @@ import axios from 'axios'
 //登陆 我的
 import Index from '@/pages/Index'
 import Modal from '@/pages/Modal'
+import RouteViews from '@/pages/RouteViews'
+import WatchView from '@/pages/WatchView';
+
+// 组件的引入
+import RouterDetail from '@/components/RouterDetail';
+import RouterList from '@/components/RouterList';
+
+
 Vue.use(Router);
 export default new Router({
   routes: [
-    { path: '/', component: Index},
-    { path: '/modal', component: Modal},
-
+    {path: '/', component: Index},
+    {path: '/modal', component: Modal},
+    {
+      path: '/router', component: RouteViews,
+      children: [
+        {path: '', component: RouterList},
+        {path: 'list', component: RouterList},
+        {path: 'detail', component: RouterDetail},
+      ]
+    },
+    {path: '/watch', component: WatchView}
   ]
 })
