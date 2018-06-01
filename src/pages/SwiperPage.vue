d62c72394fcc4fecd90d11fdcab7ec634b438ca1
zhuangmeili <zhuangmeili@auohome.com.cn>
2018-06-01 19:15:01 +0800

    awesome swiper config

--- src/pages/SwiperPage.vue
+++ src/pages/SwiperPage.vue
@@ -31,7 +31,10 @@
         swiperOption: {
           slidesPerView: 'auto',
           centeredSlides: true,
-          autoplay:true,
+          autoplay: {
+            delay: 3000,
+            disableOnInteraction: false
+          },
           loop: true,
           spaceBetween: 30,
           // pagination: {
